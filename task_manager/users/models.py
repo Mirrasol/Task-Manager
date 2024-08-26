3e667b74e677859a89c0b49b93c2a66dedb8ecd0
Elena Dobruk <elena.dobruk@yandex.ru>
2024-08-26 17:27:07 +0300

    edit users index modules, add users app to settings.py

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    password = models.CharField(min_length=3, max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from task_manager.users.models import Users
+from task_manager.users.models import User
 
 
 class IndexView(View):
