1ba7866e471140dd1bed2a2a9bd0e7c3d54a198c
Elena Dobruk <elena.dobruk@yandex.ru>
2024-08-26 17:44:14 +0300

    edit errors in models.py, edit user-app name in app.py, adjust necessary modules, run first migration

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,12 +1,11 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+class User(models.Model):
     username = models.CharField(max_length=150)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    password = models.CharField(min_length=3, max_length=200)
+    password = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -5,7 +5,7 @@ from task_manager.users.models import User
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        users = Users.objects.all()
+        users = User.objects.all()
         return render(request, 'users/index.html', context={
             'users': users
         })
