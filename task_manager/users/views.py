e5ec69611cf46af50905a9bf26ef77bd75f26e9c
Elena Dobruk <elena.dobruk@yandex.ru>
2024-08-27 19:59:01 +0300

    edit user index view

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from django.views.generic.edit import CreateView
+from django.views.generic import ListView, CreateView
 from django.contrib import messages
 from django.urls import reverse_lazy
 from task_manager.users.models import User
 from task_manager.users.forms import UserCreateForm
 
 
-class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        users = User.objects.all()
-        flash = messages.get_messages(request)
-        return render(request, 'users/index.html', context={
-            'users': users,
-            'messages': flash,
-        })
+class IndexView(ListView):
+    template_name = 'users/index.html'
+    model = User
+    context_object_name = 'users'
 
 
 class UserCreateView(CreateView):
