5b37c577ede38678af25a65fbfd6028857119a1c
Elena Dobruk <elena.dobruk@yandex.ru>
2024-09-10 15:56:46 +0300

    clean up main urls.py, add tests for updating users

--- task_manager/users/tests/tests.py
+++ task_manager/users/tests/tests.py
@@ -2,10 +2,10 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse_lazy
 
+CustomUser = get_user_model()
 
 
 class UsersTestCase(TestCase):
-    CustomUser = get_user_model()
     fixtures = ['users.json']
 
     def setUp(self):
@@ -31,9 +31,44 @@ class UsersTestCase(TestCase):
         response = self.client.post(create_url, new_user)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(CustomUser.objects.get(id=4).username, 'Devonian')
+        self.assertRedirects(response, reverse_lazy('login'))
 
-    def test_update(self):
-        pass
+    def test_update_unauthenticated(self):
+        update_url = reverse_lazy('update', kwargs={'pk': self.user2.id})
+
+        response = self.client.get(update_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+
+    def test_update_authenticated_self(self):
+        self.client.force_login(self.user1)
+        update_url = reverse_lazy('update', kwargs={'pk': self.user1.id})
+        index_url = reverse_lazy('users_index')
+
+        response = self.client.get(update_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/update.html')
+
+        updated_data = {
+            'first_name': 'Cam',
+            'last_name': 'Brian',
+            'username': 'Cambrian_1',
+            'password1': 10101,
+            'password2': 10101,
+        }
+        response = self.client.post(update_url, updated_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(CustomUser.objects.get(id=1).username, 'Cambrian_1')
+        self.assertRedirects(response, index_url)
+
+    def test_update_authenticated_others(self):
+        self.client.force_login(self.user1)
+        update_url = reverse_lazy('update', kwargs={'pk': self.user3.id})
+        index_url = reverse_lazy('users_index')
+
+        response = self.client.get(update_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, index_url)
 
     def test_delete(self):
         pass
