0d51229a7a21c7d9f87bb2bebf80b703a5b62970
Elena Dobruk <elena.dobruk@yandex.ru>
2024-09-10 16:19:07 +0300

    add tests for deleting users

--- task_manager/users/tests/tests.py
+++ task_manager/users/tests/tests.py
@@ -2,17 +2,15 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 from django.urls import reverse_lazy
 
-CustomUser = get_user_model()
-
 
 class UsersTestCase(TestCase):
     fixtures = ['users.json']
 
     def setUp(self):
         self.client = Client()
-        self.user1 = CustomUser.objects.get(pk=1)
-        self.user2 = CustomUser.objects.get(pk=2)
-        self.user3 = CustomUser.objects.get(pk=3)
+        self.user1 = get_user_model().objects.get(pk=1)
+        self.user2 = get_user_model().objects.get(pk=2)
+        self.user3 = get_user_model().objects.get(pk=3)
 
     def test_create(self):
         new_user = {
@@ -22,15 +20,14 @@ class UsersTestCase(TestCase):
             'password1': 40404,
             'password2': 40404,
         }
-        create_url = reverse_lazy('create')
 
-        response = self.client.get(create_url)
+        response = self.client.get(reverse_lazy('create'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/create.html')
 
-        response = self.client.post(create_url, new_user)
+        response = self.client.post(reverse_lazy('create'), new_user)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(CustomUser.objects.get(id=4).username, 'Devonian')
+        self.assertEqual(get_user_model().objects.get(id=4).username, 'Devonian')
         self.assertRedirects(response, reverse_lazy('login'))
 
     def test_update_unauthenticated(self):
@@ -43,7 +40,6 @@ class UsersTestCase(TestCase):
     def test_update_authenticated_self(self):
         self.client.force_login(self.user1)
         update_url = reverse_lazy('update', kwargs={'pk': self.user1.id})
-        index_url = reverse_lazy('users_index')
 
         response = self.client.get(update_url)
         self.assertEqual(response.status_code, 200)
@@ -58,17 +54,39 @@ class UsersTestCase(TestCase):
         }
         response = self.client.post(update_url, updated_data)
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(CustomUser.objects.get(id=1).username, 'Cambrian_1')
-        self.assertRedirects(response, index_url)
+        self.assertEqual(get_user_model().objects.get(id=1).username, 'Cambrian_1')
+        self.assertRedirects(response, reverse_lazy('users_index'))
 
     def test_update_authenticated_others(self):
         self.client.force_login(self.user1)
         update_url = reverse_lazy('update', kwargs={'pk': self.user3.id})
-        index_url = reverse_lazy('users_index')
 
         response = self.client.get(update_url)
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, index_url)
+        self.assertRedirects(response, reverse_lazy('users_index'))
 
-    def test_delete(self):
-        pass
+    def test_delete_unauthenticated(self):
+        self.client.logout()
+        delete_url = reverse_lazy('delete', kwargs={'pk': self.user1.id})
+
+        response = self.client.get(delete_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+
+    def test_delete_authenticated_self(self):
+        self.client.force_login(self.user1)
+        delete_url = reverse_lazy('delete', kwargs={'pk': self.user1.id})
+
+        response = self.client.post(delete_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('login'))
+        self.assertFalse(self.user1.exists())
+
+    def test_delete_authenticated_others(self):
+        self.client.force_login(self.user2)
+        delete_url = reverse_lazy('delete', kwargs={'pk': self.user3.id})
+
+        response = self.client.post(delete_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse_lazy('users_index'))
+        self.assertTrue(self.user3.exists())
