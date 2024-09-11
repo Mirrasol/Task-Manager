f87e29fbcf36ee8c6ef29387be8b38933ba11982
Elena Dobruk <elena.dobruk@yandex.ru>
2024-09-11 15:58:27 +0300

    add status model, make first migration, update localization files

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=150, verbose_name=_('Name'), unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
