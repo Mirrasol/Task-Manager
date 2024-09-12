b212c040d8587aff676d20f5bd8fd18a33a3bc75
Elena Dobruk <elena.dobruk@yandex.ru>
2024-09-12 11:57:31 +0300

    add fixture for statuses tests

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Status
 
-# Create your tests here.
+
+class StatusesTestCase(TestCase):
+    fixtures = ['statuses.json']
+
+    def setUp(self):
+        self.client = Client()
+        self.status1 = Status.objects.get(pk=1)
+        self.status2 = Status.objects.get(pk=2)
+        self.status3 = Status.objects.get(pk=3)
