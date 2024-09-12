1c9295e2f9bb83d5313b9f6559f32b437effbf01
Elena Dobruk <elena.dobruk@yandex.ru>
2024-09-12 13:41:01 +0300

    create tasks model, make initial migration, update tasks index view

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from task_manager.statuses.models import Status
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name=_('Name'),
+        unique=True,
+    )
+    description = models.TextField(
+        verbose_name=_('Description'),
+        null=True,
+        blank=True,
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        verbose_name=_('Status'),
+    )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        related_name='author',
+        verbose_name=_('Author'),
+    )
+    executor = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.PROTECT,
+        related_name='executor',
+        verbose_name=_('Executor'),
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from task_manager.tasks.models import Task
 from task_manager.mixins import AuthenticatedMixin
 
 
-class IndexView(AuthenticatedMixin, TemplateView):
-    template_name = 'statuses/index.html'
+class IndexView(AuthenticatedMixin, ListView):
+    template_name = 'tasks/index.html'
+    model = Task
     context_object_name = 'tasks'
+    ordering = ['id']
