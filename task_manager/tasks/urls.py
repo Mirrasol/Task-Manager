cd67843e706cb7c93e1928014f9e0fccd8e38efb
Elena Dobruk <elena.dobruk@yandex.ru>
2024-09-12 16:08:49 +0300

    add task create route

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from task_manager.tasks.views import IndexView
+from task_manager.tasks.views import IndexView, TaskCreateView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='tasks_index'),
+    path('create/', TaskCreateView.as_view(), name='task_create'),
 ]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from task_manager.tasks.models import Task
 from task_manager.mixins import AuthenticatedMixin
+from task_manager.tasks.forms import TaskCreateForm
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
 
 
 class IndexView(AuthenticatedMixin, ListView):
@@ -8,3 +12,16 @@ class IndexView(AuthenticatedMixin, ListView):
     model = Task
     context_object_name = 'tasks'
     ordering = ['id']
+
+
+class TaskCreateView(AuthenticatedMixin, SuccessMessageMixin, CreateView):
+    template_name = 'tasks/create.html'
+    model = Task
+    form_class = TaskCreateForm
+    success_url = reverse_lazy('tasks_index')
+    success_message = _('Task has been created successfully')
+
+    def form_valid(self, form):
+        task = form.save(commit=False)
+        task.author = self.request.user
+        return super().form_valid(form)
